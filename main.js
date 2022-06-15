d62fccc006ab5572a0fc660e4c8175e2df51e7da
ProdicaI <73436076+ProdicaI@users.noreply.github.com>
2022-06-15 16:37:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,20 @@ function setup() {
   canvas = createCanvas(1240, 336);
   canvas.parent("canvas");
   instializeInSetup(mario);
+
+  let video = createCapture(VIDEO);
+  video.size(1240, 336);
+  video.parent("game_console");
+
+  let poseNet = ml5.poseNet(video, () => console.log("Model Loaded!"));
+
+  poseNet.on("pose", (res) => {
+    if (res.length > 0) {
+      noseX = res[0].pose.nose.x;
+      noseY = res[0].pose.nose.y;
+      console.log(`NoseX: ${noseX}\nNoseY: ${noseY}`);
+    }
+  });
 }
 
 function draw() {
